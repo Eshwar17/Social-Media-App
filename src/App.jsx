539c01c3dd2ffcf842af4c99a34ca30a2f5be69a
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-11-26 15:10:48 +0530

    Added Header, Footer, Sidebar components and added styling accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 function App() {
   
   return (
-    <>
-      <div>Placeholder</div>
-    </>
+  <div className='app-container'>
+    <Sidebar />
+    <div className='content'><Header />
+    <Footer /></div></div>
   )
 }
 
